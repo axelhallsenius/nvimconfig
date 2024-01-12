950eeba9197965376b56a7d59d44e2af518eac08
Axel Hallsenius <axelhallsenius@gmail.com>
2024-01-12 10:59:35 +0100

    debug frame

---
+++ nvim/lua/plugins/debugging.lua
@@ -0,0 +1,29 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+	},
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<Leader>db", function()
+			dap.toggle_breakpoint()
+		end)
+		vim.keymap.set("n", "<Leader>dc", function()
+			dap.continue()
+		end)
+	end,
+}
