8c39c19973732ec753ebf48d34a5a742f3c57405
Axel Hallsenius <axelhallsenius@gmail.com>
2024-01-12 13:34:26 +0100

    hop replacing f and t

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -5,20 +5,23 @@ return {
 		-- you can configure Hop the way you like here; see :h hop-config
 		local hop = require("hop")
 
-		hop.setup({ keys = "etovxqpdygfblzhckisuran" })
+		hop.setup({ keys = "asdfghjklqwertyuiop" })
 
 		local directions = require("hop.hint").HintDirection
-		vim.keymap.set("n", "<leader>f", function()
+		vim.keymap.set({"n", "v"}, "f", function()
 			hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
 		end, { remap = true })
-		vim.keymap.set("n", "<leader>F", function()
+		vim.keymap.set({"n", "v"}, "F", function()
 			hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
 		end, { remap = true })
-		vim.keymap.set("n", "<leader>t", function()
+		vim.keymap.set({"n", "v"}, "t", function()
 			hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
 		end, { remap = true })
-		vim.keymap.set("n", "<leader>T", function()
+		vim.keymap.set({"n", "v"}, "T", function()
 			hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+		end, { remap = true })
+		vim.keymap.set({"n", "v"}, "ä", function()
+			hop.hint_char1()
 		end, { remap = true })
 	end,
 }
