0da5ea0afa66c01450e917537c36affeecc5d5a8
Axel Hallsenius <axelhallsenius@gmail.com>
2025-01-21 14:57:57 +0100

    registers

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -53,13 +53,13 @@ vim.keymap.set('n', 'ö', ':')
 vim.keymap.set('n', ';', ':')
 
 
-vim.keymap.set('n', '<leader>p', '"*p')
-vim.keymap.set('n', '<leader>P', '"*P')
-vim.keymap.set('v', '<leader>p', '"*p')
-vim.keymap.set('v', '<leader>P', '"*P')
+vim.keymap.set('n', '<leader>p', '"+p')
+vim.keymap.set('n', '<leader>P', '"+P')
+vim.keymap.set('v', '<leader>p', '"+p')
+vim.keymap.set('v', '<leader>P', '"+P')
 
 --why no work?
-vim.keymap.set('n', '<leader>y', '"*y')
-vim.keymap.set('n', '<leader>Y', '"*Y')
-vim.keymap.set('v', '<leader>y', '"*y')
-vim.keymap.set('v', '<leader>Y', '"*Y')
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>Y', '"+Y')
