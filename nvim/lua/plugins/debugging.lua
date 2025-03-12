347c5547b45f38a22be102bcceee5869f4cb76d9
Axel Hallsenius <axelhallsenius@gmail.com>
2025-03-12 14:57:27 +0100

    updated and fixed broken features

--- nvim/lua/plugins/debugging.lua
+++ nvim/lua/plugins/debugging.lua
@@ -2,6 +2,7 @@ return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
 		"rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
 	},
 	config = function()
 		local dap, dapui = require("dap"), require("dapui")
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,16 +1,18 @@
 return { -- wraps cli tools for formatting into kinda pseudo linters, to be used in the editor
 	"nvimtools/none-ls.nvim", -- configure formatters & linters
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
 	config = function()
 		local null_ls = require("null-ls")
 
 		null_ls.setup({
 			sources = {
+        require("none-ls.diagnostics.eslint_d"),
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
-
-				null_ls.builtins.diagnostics.eslint_d,
 			},
 		})
 		vim.keymap.set("n", "<leader>=", vim.lsp.buf.format, {})
