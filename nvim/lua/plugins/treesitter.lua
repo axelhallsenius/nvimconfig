9ddd85e1a8e7a88610e020a606482e494201087a
Axel Hallsenius <axelhallsenius@gmail.com>
2025-04-03 20:12:56 +0200

    fixed ts thing

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   opts = {
-    ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+    ensure_installed = { 'bash', 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'cmake' },
     auto_install = true,
     highlight = {
       enable = true,
@@ -18,6 +18,5 @@ return {
     require('nvim-treesitter.configs').setup(opts)
   end,
 }
-
 
 
